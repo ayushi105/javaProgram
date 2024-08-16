13ad13a0614b4894c2e24aebf2a8c69806a08755
ayushi105 <104146130+ayushi105@users.noreply.github.com>
2024-08-16 12:44:31 +0530

    Update fjp.java

--- fjp.java
+++ fjp.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     static void method(){
        System.out.println("Hello Java Method");
+        System.out.println("Hello Modified Java Method");
      }   
     public static void main(String[] args) {
         System.out.println("Hello Java World");
